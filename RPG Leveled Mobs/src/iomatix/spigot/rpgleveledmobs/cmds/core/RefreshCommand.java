bf378ae3f531a200b69f50cc6af1a24cc5c2be08
iomatix <mateuszwypchlak@gmail.com>
2019-09-30 17:32:30 +0200

    [2.5.2] - refresh command reworked - force refresh

--- RPG Leveled Mobs/src/iomatix/spigot/rpgleveledmobs/cmds/core/RefreshCommand.java
+++ RPG Leveled Mobs/src/iomatix/spigot/rpgleveledmobs/cmds/core/RefreshCommand.java
@@ -103,25 +103,9 @@ public class RefreshCommand implements RPGlvlmobsCommand {
 	}
 
 	public static void LoadTheMetaData(LivingEntity livingEntity) {
-		if (livingEntity.hasMetadata(MetaTag.RPGmob.toString()) && livingEntity.hasMetadata(MetaTag.Level.toString())
-				&& livingEntity.hasMetadata(MetaTag.ExpMod.toString())
-				&& livingEntity.hasMetadata(MetaTag.MoneyDrop.toString())
-				&& livingEntity.hasMetadata(MetaTag.MoneyMod.toString())) {
-			final int level = livingEntity.getMetadata(MetaTag.Level.toString()).get(0).asInt();
-			final double moneyMod = livingEntity.getMetadata(MetaTag.MoneyMod.toString()).get(0).asDouble();
-			final double moneyDrop = livingEntity.getMetadata(MetaTag.MoneyDrop.toString()).get(0).asDouble();
-			final double ExpMod = livingEntity.getMetadata(MetaTag.ExpMod.toString()).get(0).asDouble();
-			if (!(level > 0) || !(ExpMod > 0) || !(moneyDrop > 0) || !(moneyMod > 0)) {
-				livingEntity.removeMetadata(MetaTag.RPGmob.toString(), (Plugin) Main.RPGMobs);
-				LoadMobMetaData(livingEntity, CreatureSpawnEvent.SpawnReason.DEFAULT);
-				return;
-			}
-			RefreshMetaToLevel(livingEntity);
-		} else {
 			if (livingEntity.hasMetadata(MetaTag.RPGmob.toString()))
 				livingEntity.removeMetadata(MetaTag.RPGmob.toString(), (Plugin) Main.RPGMobs);
-			LoadMobMetaData(livingEntity, CreatureSpawnEvent.SpawnReason.DEFAULT);
-		}
+			LoadMobMetaData(livingEntity, CreatureSpawnEvent.SpawnReason.DEFAULT);	
 	}
 
 	public static void LoadMobMetaData(LivingEntity livingEntity, CreatureSpawnEvent.SpawnReason SpawnReason) {
