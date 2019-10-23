3c500ddca20b40ccc90d2d42b905a5d1c8757819
iomatix <mateuszwypchlak@gmail.com>
2019-10-23 23:34:24 +0200

    [2.6.2] - BiasedRandom update

--- RPG Leveled Mobs/src/iomatix/spigot/rpgleveledmobs/tools/BiasedRandom.java
+++ RPG Leveled Mobs/src/iomatix/spigot/rpgleveledmobs/tools/BiasedRandom.java
@@ -1,14 +1,25 @@
 package iomatix.spigot.rpgleveledmobs.tools;
 
 public class BiasedRandom {
-	public static int randomIntAlter(int min,int max){
+	//old formula
+	public static int randomIntOld(int min,int max){
 	    return (int) Math.floor(Math.abs(Math.random()-Math.random())*(1+max-min)+min);
 	}
 	
-	public static double randomDoubleAlter(double min,double max){
+	public static double randomDoubleOld(double min,double max){
 	    return Math.abs(Math.random()-Math.random())*(1+max-min)+min;
 	}
 	
+	//PoW formula
+	public static int randomAlter(int min,int max){
+		 return (int) Math.floor( (Math.random() * (1+Math.random() * ((max-min)+min)) ));
+	}
+
+	public static double randomDoubleAlter(double min,double max){
+	    return  Math.random() * (1+Math.random() * ((max-min)+min));
+	}
+	
+	//sqrt formula
 	public static int randomInt(int min,int max){
 		 return (int) Math.floor((1 - Math.sqrt(1 - Math.random()))*((1+max-min)+min));
 	}
