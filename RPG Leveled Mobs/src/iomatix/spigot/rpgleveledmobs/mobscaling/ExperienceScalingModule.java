21991bc819c6c9ba18c390a6f0e8686daf00ab51
iomatix <mateuszwypchlak@gmail.com>
2019-11-28 17:53:47 +0100

    2.7.0 - Handled MMOCore

--- RPG Leveled Mobs/src/iomatix/spigot/rpgleveledmobs/mobscaling/ExperienceScalingModule.java
+++ RPG Leveled Mobs/src/iomatix/spigot/rpgleveledmobs/mobscaling/ExperienceScalingModule.java
@@ -17,21 +17,19 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.event.Listener;
 import org.bukkit.metadata.MetadataValue;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.Bukkit;
 
 import com.sucy.skill.api.enums.ExpSource;
 import com.sucy.skill.api.event.PlayerExperienceGainEvent;
-import com.sucy.skill.api.event.PlayerLevelUpEvent;
 import com.sucy.skill.SkillAPI;
 
 import iomatix.spigot.rpgleveledmobs.events.RPGMobsGainExperience;
-import iomatix.spigot.rpgleveledmobs.events.RPGMobsGainMoney;
 import iomatix.spigot.rpgleveledmobs.Main;
 import iomatix.spigot.rpgleveledmobs.cmds.core.RefreshCommand;
 import iomatix.spigot.rpgleveledmobs.logging.LogsModule;
 import iomatix.spigot.rpgleveledmobs.tools.MetaTag;
+import net.Indyuce.mmocore.MMOCore;
 
 public class ExperienceScalingModule {
 	private boolean SkillAPIHandled;
@@ -49,6 +47,11 @@ public class ExperienceScalingModule {
 			new SkillAPIHandler();
 			experienceHandled = true;
 			SkillAPIHandled = true;
+		}
+		if (Bukkit.getPluginManager().getPlugin("MMOCore") != null) {
+			experienceHandled = true;
+			new MMOCoreHandler();
+			LogsModule.info("Found MMOCore, Enabling MMOCore Experience Mod.");
 		}
 		if (!experienceHandled) {
 			LogsModule.info("No Leveling Plugins Found, Enabling Vanilla Experience Mod.");
@@ -98,6 +101,121 @@ public class ExperienceScalingModule {
 		public HeroesHandler() {
 			Bukkit.getPluginManager().registerEvents((Listener) this, (Plugin) Main.RPGMobs);
 		}
+	}
+
+	private class MMOCoreHandler implements Listener {
+		public MMOCoreHandler() {
+			Bukkit.getPluginManager().registerEvents((Listener) this, (Plugin) Main.RPGMobs);
+		}
+		
+		@EventHandler(priority = EventPriority.HIGHEST)
+		public void onEntityDeath(final EntityDeathEvent event) {
+			 if (event.getEntity().hasMetadata(MetaTag.RPGmob.toString())
+					&& event.getEntity().hasMetadata(MetaTag.Level.toString())
+					&& (event.getEntity().hasMetadata(MetaTag.ExpMod.toString())
+							&& event.getEntity().hasMetadata(MetaTag.ExpAddon.toString()))) {
+				Player tempKiller = null;
+				if (event.getEntity().getKiller() == null
+						&& event.getEntity().getLastDamageCause() instanceof EntityDamageByEntityEvent) {
+					final EntityDamageByEntityEvent nEvent = (EntityDamageByEntityEvent) event.getEntity()
+							.getLastDamageCause();
+					final Entity damager = nEvent.getDamager();
+					if (damager != null && damager instanceof Tameable) {
+						final Tameable thePet = (Tameable) damager;
+						if (thePet.isTamed()) {
+
+							tempKiller = (Player) ((Tameable) damager).getOwner();
+						}
+						
+					}
+				} else if (event.getEntity().getKiller() != null) {
+					tempKiller = event.getEntity().getKiller();
+				}
+
+				final Player killer = tempKiller;
+				if (killer != null) {
+
+					final int mobLevel = event.getEntity().getMetadata(MetaTag.Level.toString()).get(0).asInt();
+					final double expFinal = mobLevel
+							* event.getEntity().getMetadata(MetaTag.ExpMod.toString()).get(0).asDouble();
+					final double expAddonFinal = mobLevel
+							* event.getEntity().getMetadata(MetaTag.ExpAddon.toString()).get(0).asDouble();
+					if (killer.hasMetadata(MetaTag.RecentKill.toString())) {
+						((LinkedList) killer.getMetadata(MetaTag.RecentKill.toString()).get(0).value())
+								.addLast(expFinal);
+					} else {
+						final LinkedList<Double> q = new LinkedList<Double>();
+						q.addLast(expFinal);
+						killer.setMetadata(MetaTag.RecentKill.toString(),
+								(MetadataValue) new FixedMetadataValue((Plugin) Main.RPGMobs, (Object) q));
+					}
+					if (killer.hasMetadata(MetaTag.RecentKillAddon.toString())) {
+						((LinkedList) killer.getMetadata(MetaTag.RecentKillAddon.toString()).get(0).value())
+								.addLast(expAddonFinal);
+					} else {
+						final LinkedList<Double> q = new LinkedList<Double>();
+						q.addLast(expAddonFinal);
+						killer.setMetadata(MetaTag.RecentKillAddon.toString(),
+								(MetadataValue) new FixedMetadataValue((Plugin) Main.RPGMobs, (Object) q));
+					}
+
+				}
+			}
+		}
+		
+		@EventHandler(priority = EventPriority.HIGHEST)
+		public void onPlayerGainExp(final net.Indyuce.mmocore.api.event.PlayerExperienceGainEvent event) {
+			final Player killer = event.getPlayer();
+			if ( !killer.hasMetadata(MetaTag.RecentKill.toString())) {
+				return;
+			}
+			try {
+				final double expModifier = (double) ((LinkedList) killer.getMetadata(MetaTag.RecentKill.toString())
+						.get(0).value()).removeFirst();
+				final double expAddon = (double) ((LinkedList) killer.getMetadata(MetaTag.RecentKillAddon.toString())
+						.get(0).value()).removeFirst();
+				if (expModifier != 0) {
+
+					final double theExp = event.getExperience() + event.getExperience() * expModifier + expAddon;
+					RPGMobsGainExperience gainExperienceEvent = new RPGMobsGainExperience(theExp, killer);
+					Bukkit.getPluginManager().callEvent(gainExperienceEvent);
+					if (!(gainExperienceEvent.isCancelled())) {
+						event.setExperience((int) Math.floor(theExp));
+					} else {
+						event.setCancelled(true);
+					}
+				}
+			} catch (Exception e) {
+				return;
+			}
+			
+		}
+		
+		@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+		public void onPlayerGainLevel(final net.Indyuce.mmocore.api.event.PlayerLevelUpEvent event) {
+			final int newLevel = event.getNewLevel();
+			final Player who = event.getPlayer();
+			for (final World world : Bukkit.getWorlds()) {
+				for (final Entity ent : world.getEntities()) {
+					if (ent instanceof Tameable) {
+						final Tameable thePet = (Tameable) ent;
+						if (thePet.isTamed() && thePet.getOwner() != null ) {
+							if (thePet.getOwner().getName().equals(who.getName())) {
+								if (thePet.hasMetadata(MetaTag.Level.toString())) {
+									thePet.removeMetadata(MetaTag.Level.toString(), (Plugin) Main.RPGMobs);
+								}
+								thePet.setMetadata(MetaTag.Level.toString(),
+										(MetadataValue) new FixedMetadataValue((Plugin) Main.RPGMobs,
+												(Object) newLevel));
+								RefreshCommand.RefreshMetaToLevel((LivingEntity) thePet);
+							}
+						}
+					}
+				}
+			}
+
+		}	
+		
 	}
 
 	private class SkillAPIHandler implements Listener {
@@ -168,7 +286,7 @@ public class ExperienceScalingModule {
 		}
 
 		@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-		public void onPlayerGainExp(final PlayerExperienceGainEvent event) {
+		public void onPlayerGainExp(final com.sucy.skill.api.event.PlayerExperienceGainEvent event) {
 			final Player killer = event.getPlayerData().getPlayer();
 			if (event.getSource() != ExpSource.MOB || !killer.hasMetadata(MetaTag.RecentKill.toString())) {
 				return;
@@ -195,8 +313,8 @@ public class ExperienceScalingModule {
 			}
 		}
 
-		@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-		public void onPlayerGainLevel(final PlayerLevelUpEvent event) {
+		@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+		public void onPlayerGainLevel(final com.sucy.skill.api.event.PlayerLevelUpEvent event) {
 			final int newLevel = event.getLevel();
 			final Player who = event.getPlayerData().getPlayer();
 			for (final World world : Bukkit.getWorlds()) {
--- RPG Leveled Mobs/src/iomatix/spigot/rpgleveledmobs/mobscaling/StatsScalingModule.java
+++ RPG Leveled Mobs/src/iomatix/spigot/rpgleveledmobs/mobscaling/StatsScalingModule.java
@@ -32,6 +32,11 @@ public class StatsScalingModule {
 			new SkillAPIHandler();
 			LogsModule.info("Found SkillAPI, Enabling SkillAPI Scaling Mod.");
 		}
+		if (Bukkit.getPluginManager().getPlugin("MMOCore") != null) {
+			scalingModified = true;
+			new MMOCoreHandler();
+			LogsModule.info("Found MMOCore, Enabling MMOCore Scaling Mod.");
+		}
 		if (!scalingModified) {
 			new VanillaHandler();
 			LogsModule.info("Enabling Vanilla Scaling Mod.");
@@ -86,7 +91,7 @@ public class StatsScalingModule {
 			Bukkit.getPluginManager().registerEvents((Listener) this, (Plugin) Main.RPGMobs);
 		}
 
-		@EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+		@EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
 		public void onEntityDamage(final EntityDamageByEntityEvent event) {
 			if (event.getDamager() instanceof Projectile
 					&& ((Projectile) event.getDamager()).getShooter() instanceof Monster) {
@@ -104,12 +109,42 @@ public class StatsScalingModule {
 				final double defenseMod = StatsScalingModule.this.getDefenseMod(event.getEntity());
 				final double defenseAddon = StatsScalingModule.this.getDefenseAddon(event.getEntity());
 				final int level = StatsScalingModule.this.getLevel(event.getEntity());
-				final double output = event.getDamage()
-						* (1 - (Math
-								.min(20.0,
-										Math.max((level * defenseMod + defenseAddon * level * defenseMod) / 5,
-												(level * defenseAddon + defenseAddon * level * defenseMod) - (event.getDamage() / 2))))
-								/ 25);
+				final double output = event.getDamage() * (1 - (Math.min(20.0,
+						Math.max((level * defenseMod + defenseAddon * level * defenseMod) / 5,
+								(level * defenseAddon + defenseAddon * level * defenseMod) - (event.getDamage() / 2))))
+						/ 25);
+				event.setDamage(output);
+			}
+		}
+	}
+
+	private class MMOCoreHandler implements Listener {
+		public MMOCoreHandler() {
+			Bukkit.getPluginManager().registerEvents((Listener) this, (Plugin) Main.RPGMobs);
+		}
+
+		@EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
+		public void onEntityDamage(final EntityDamageByEntityEvent event) {
+			if (event.getDamager() instanceof Projectile
+					&& ((Projectile) event.getDamager()).getShooter() instanceof Monster) {
+				StatsScalingModule.this.onLeveledMobShoot((Entity) ((Projectile) event.getDamager()).getShooter(),
+						event.getDamager());
+			}
+			if (isDamageModded(event.getDamager())) {
+				final double damageMod = StatsScalingModule.this.getDamageMod(event.getDamager());
+				final double damageAddon = StatsScalingModule.this.getDamageAddon(event.getDamager());
+				final int level = StatsScalingModule.this.getLevel(event.getDamager());
+				event.setDamage(event.getDamage() + event.getDamage() * damageMod * level + damageAddon * level);
+			}
+
+			if (isDefenseModded(event.getEntity())) {
+				final double defenseMod = StatsScalingModule.this.getDefenseMod(event.getEntity());
+				final double defenseAddon = StatsScalingModule.this.getDefenseAddon(event.getEntity());
+				final int level = StatsScalingModule.this.getLevel(event.getEntity());
+				final double output = event.getDamage() * (1 - (Math.min(20.0,
+						Math.max((level * defenseMod + defenseAddon * level * defenseMod) / 5,
+								(level * defenseAddon + defenseAddon * level * defenseMod) - (event.getDamage() / 2))))
+						/ 25);
 				event.setDamage(output);
 			}
 		}
@@ -140,7 +175,9 @@ public class StatsScalingModule {
 				final double defenseAddon = StatsScalingModule.this.getDefenseAddon(event.getEntity());
 				final double damage = event.getDamage();
 				final double output = damage * (1 - (Math.min(20.0,
-						Math.max( defenseAddon*level+defenseAddon*level * defenseMod / 5, (defenseAddon * level + defenseMod * defenseAddon * level) - (event.getDamage() / 2)))) / 25);
+						Math.max(defenseAddon * level + defenseAddon * level * defenseMod / 5,
+								(defenseAddon * level + defenseMod * defenseAddon * level) - (event.getDamage() / 2))))
+						/ 25);
 
 				event.setDamage(output);
 
@@ -153,13 +190,13 @@ public class StatsScalingModule {
 			Bukkit.getPluginManager().registerEvents((Listener) this, (Plugin) Main.RPGMobs);
 		}
 
-		@EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+		@EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
 		public void onPhysicalDamage(final PhysicalDamageEvent event) {
 			if (StatsScalingModule.this.isDamageModded((Entity) event.getDamager())) {
 				final int level = StatsScalingModule.this.getLevel((Entity) event.getDamager());
 				final double damageMod = StatsScalingModule.this.getDamageMod((Entity) event.getDamager());
 				final double damageAddon = StatsScalingModule.this.getDamageAddon((Entity) event.getDamager());
-				event.setDamage(event.getDamage() + level*damageAddon+level * damageMod * event.getDamage());
+				event.setDamage(event.getDamage() + level * damageAddon + level * damageMod * event.getDamage());
 			}
 
 			if (StatsScalingModule.this.isDefenseModded((Entity) event.getTarget())) {
@@ -167,7 +204,9 @@ public class StatsScalingModule {
 				final double defenseMod = StatsScalingModule.this.getDefenseMod((Entity) event.getTarget());
 				final double defenseAddon = StatsScalingModule.this.getDefenseAddon((Entity) event.getTarget());
 				final double output = event.getDamage() * (1 - (Math.min(20.0,
-						Math.max(defenseAddon*level+defenseAddon*level * defenseMod / 5, (defenseAddon*level+defenseAddon*level * defenseMod) - (event.getDamage() / 2)))) / 25);
+						Math.max(defenseAddon * level + defenseAddon * level * defenseMod / 5,
+								(defenseAddon * level + defenseAddon * level * defenseMod) - (event.getDamage() / 2))))
+						/ 25);
 
 				event.setDamage(output);
 
@@ -175,14 +214,16 @@ public class StatsScalingModule {
 
 		}
 
-		@EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+		@EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
 		public void onSkillDamage(final SkillDamageEvent event) {
 			if (StatsScalingModule.this.isDefenseModded((Entity) event.getTarget())) {
 				final int level = StatsScalingModule.this.getLevel((Entity) event.getTarget());
 				final double defenseMod = StatsScalingModule.this.getDefenseMod((Entity) event.getTarget());
 				final double defenseAddon = StatsScalingModule.this.getDefenseAddon((Entity) event.getTarget());
 				final double output = event.getDamage() * (1 - (Math.min(20.0,
-						Math.max(defenseAddon*level+defenseAddon*level * defenseMod / 5, (defenseAddon*level+defenseAddon*level * defenseMod) - (event.getDamage() / 2)))) / 25);
+						Math.max(defenseAddon * level + defenseAddon * level * defenseMod / 5,
+								(defenseAddon * level + defenseAddon * level * defenseMod) - (event.getDamage() / 2))))
+						/ 25);
 
 				event.setDamage(output);
 
